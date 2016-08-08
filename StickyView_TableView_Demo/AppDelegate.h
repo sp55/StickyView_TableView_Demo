c22f0a18ea48c275ff97e9d7c1bdf7246d43e653
sp55 <1172242456@qq.com>
2016-08-08 13:52:31 +0800

    TableView向上滚动的时候   有一部分粘在上面
    TableView向上滚动的时候   有一部分粘在上面
    向下滚动的时候    跟着滚下来

---
+++ StickyView_TableView_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StickyView_TableView_Demo
+//
+//  Created by admin on 16/8/8.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
